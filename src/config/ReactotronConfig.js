d2fed463e2b40c86e028a7005dcd2a90a2df4144
Leonardo Diego <leoosinhu@gmail.com>
2020-01-19 19:47:36 -0300

    Configuracoes gerais

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,13 @@
+import Reacttotron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
+
+if (process.env.NODE_ENV === 'development') {
+  const tron = Reacttotron.configure()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
+    .connect();
+
+  tron.clear();
+  console.tron = tron;
+}
